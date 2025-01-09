d7cea0d55b877c865bc8d32b22bf97a94ff2fc16
ACBorgia <fakeilovewash@gmail.com>
2025-01-09 14:12:03 +0100

    Fix du proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,14 @@
+// frontend/vue.config.js
+
 module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
   chainWebpack: (config) => {
     config.plugin("define").tap((definitions) => {
       Object.assign(definitions[0]["process.env"], {
